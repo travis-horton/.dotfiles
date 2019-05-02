28d62851876b2990657b1898e0debc35dad363a2
Travis Horton <travis@travish.com>
2019-05-02 16:30:26 -0400

    added a .vim/swp/ mkdir

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#create .vim/swp/ (-p makes parent dirs if necessary)
+mkdir -p .vim
+
 ln -sf "$HOME/dotfiles/.bashrc" ~/.bashrc 
 ln -sf "$HOME/dotfiles/.bash_profile" ~/.bash_profile
 ln -sf "$HOME/dotfiles/.vimrc" ~/.vimrc
