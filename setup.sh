b190334094eff5fe642da3ee9f0ea7908f978f6f
kiddspazz <travis@travish.com>
2019-07-04 16:31:53 +0000

    Changed file directories to reflect .dotfiles instead of dotfiles. Changed setup.sh to executable. Changed location of .vim/ to HOME/.vim/

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #create .vim/swp/ (-p makes parent dirs if necessary)
-mkdir -p .vim/swp
+mkdir -p $HOME/vim/swp
 
-ln -sf "$HOME/dotfiles/.bashrc" ~/.bashrc 
-ln -sf "$HOME/dotfiles/.bash_profile" ~/.bash_profile
-ln -sf "$HOME/dotfiles/.vimrc" ~/.vimrc
-ln -sf "$HOME/dotfiles/.gitconfig" ~/.gitconfig
-ln -sf "$HOME/dotfiles/.npmrc" ~/.npmrc
+ln -sf "$HOME/.dotfiles/.bashrc" ~/.bashrc 
+ln -sf "$HOME/.dotfiles/.bash_profile" ~/.bash_profile
+ln -sf "$HOME/.dotfiles/.vimrc" ~/.vimrc
+ln -sf "$HOME/.dotfiles/.gitconfig" ~/.gitconfig
+ln -sf "$HOME/.dotfiles/.npmrc" ~/.npmrc
