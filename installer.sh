2ca0f718eafd2db2725ae1f080a6bac52d2d8c3a
kiddspazz <travis@travish.com>
2022-12-30 09:48:14 -0700

    Make the statuses clearer and easier to read

--- installer.sh
+++ installer.sh
@@ -1,49 +1,63 @@
-echo '\n\n------------------------------'
-echo '------- SETUP SCRIPT ---------'
-echo '------------------------------\n'
+# Print a full horizontal line of dashes
+printf '\n\n%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -;
+echo "------------------------------";
+echo "------- SETUP SCRIPT ---------";
+echo "------------------------------";
 
-echo '------------------------------'
-echo '------- INSTALL XCODE --------'
-echo '------------------------------\n'
-xcode-select --install
+echo "------------------------------";
+echo "------- INSTALL XCODE --------";
+echo "------------------------------";
+set -x;
+xcode-select --install;
+set +x;
 
-echo '---------------------------------'
-echo '------- INSTALL HOMEBREW --------'
-echo '---------------------------------\n'
+echo "\n---------------------------------";
+echo "------- INSTALL HOMEBREW --------";
+echo "---------------------------------";
+# I don't need to set -x here because homebrew install has good status updates
 /bin/bash -c "$(curl -fsSL \
-  https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
 
-echo '-----------------------------------------'
-echo '------- SYMLINK ZSH CONFIG STUFF --------'
-echo '-----------------------------------------\n'
-ln -vsf ${HOME}/.dotfiles/.zprofile ${HOME}/.zprofile
-ln -vsf ${HOME}/.dotfiles/.zshenv ${HOME}/.zshenv
-ln -vsf ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
+echo "\n-----------------------------------------";
+echo "------- SYMLINK ZSH CONFIG STUFF --------";
+echo "-----------------------------------------";
+set -x;
+ln -vsf ${HOME}/.dotfiles/.zprofile ${HOME}/.zprofile;
+ln -vsf ${HOME}/.dotfiles/.zshenv ${HOME}/.zshenv;
+ln -vsf ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc;
+set +x;
 
-echo '------------------------------------------------'
-echo '------- INSTALL BREW PACKAGES FROM LIST --------'
-echo '------------------------------------------------\n'
-xargs brew install < ${HOME}/.dotfiles/brew_packages_to_install.txt
+echo "\n------------------------------------------------";
+echo "------- INSTALL BREW PACKAGES FROM LIST:";
+cat ${HOME}/.dotfiles/brew_packages_to_install.txt;
+echo "------------------------------------------------";
+set -x;
+xargs brew install < ${HOME}/.dotfiles/brew_packages_to_install.txt;
+set +x;
 
-echo '--------------------------------------'
-echo '------- UPDATE ALL THE THINGS --------'
-echo '--------------------------------------\n'
-brew upgrade
+echo "\n---------------------------------------------------------";
+echo "------- UPDATE ALL THE THINGS (run brew upgrade) --------";
+echo "---------------------------------------------------------";
+set -x;
+brew upgrade;
+set +x;
 
-echo '-----------------------------'
-echo '------- INSTALL YARN --------'
-echo '-----------------------------\n'
-npm install --global yarn
+echo "\n-----------------------------";
+echo "------- INSTALL YARN --------";
+echo "-----------------------------";
+set -x;
+npm install --global yarn;
+set +x;
 
 # check git command
 if type git; then
   : # You have git command. No Problem.
 else
-  echo 'Please install git or update your path to include the git executable!'
-  exit 1
+  echo "Please install git or update your path to include the git executable!";
+  exit 1;
 fi
 
-echo '------------------------------'
-echo '------- RUN VIM SETUP --------'
-echo '------------------------------\n'
-/bin/bash ${HOME}/.dotfiles/nvim_setup.sh
+echo "\n------------------------------";
+echo "------- RUN VIM SETUP --------";
+echo "------------------------------\n";
+/bin/zsh ${HOME}/.dotfiles/nvim_setup.sh;
--- nvim_setup.sh
+++ nvim_setup.sh
@@ -1,154 +1,206 @@
-echo '\n\n----------------------------'
-echo '------- NVIM SETUP ---------'
-echo '----------------------------'
+# Print a full horizontal line of dashes
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -;
+echo "----------------------------";
+echo "------- NVIM SETUP ---------";
+echo "----------------------------";
 
-echo '-------------------------------------------------------'
-echo '------- MAKE NVIM DIR IN ${HOME}/.CONFIG/NVIM ---------'
-echo '-------------------------------------------------------\n'
+echo "------------------------------------------------------";
+echo "------- MAKE NVIM DIR IN \$HOME/.CONFIG/NVIM ----------";
+echo "------------------------------------------------------";
+set -x;
 mkdir -p ${HOME}/.config/nvim;
-echo '--------------------------------------------------------------'
-echo '------- SYMLINK CONFIG INIT.VIM TO DOTFILES INIT.VIM ---------'
-echo '--------------------------------------------------------------\n'
+set +x;
+
+echo "\n--------------------------------------------------------------";
+echo "------- SYMLINK CONFIG INIT.VIM TO DOTFILES INIT.VIM ---------";
+echo "--------------------------------------------------------------";
+set -x;
 ln -vsf ${HOME}/.dotfiles/.config/nvim/init.vim ${HOME}/.config/nvim/init.vim;
-echo '----------------------------------------------------------------------'
-echo '------- SYMLINK CONFIG COC-SETTINGS TO DOTFILES COC-SETTINGS ---------'
-echo '----------------------------------------------------------------------\n'
+set +x;
+
+echo "\n----------------------------------------------------------------------";
+echo "------- SYMLINK CONFIG COC-SETTINGS TO DOTFILES COC-SETTINGS ---------";
+echo "----------------------------------------------------------------------";
+set -x;
 ln -vsf ${HOME}/.dotfiles/.config/nvim/coc-settings.json \
   ${HOME}/.config/nvim/coc-settings.json;
 
 PLUGIN_DIR=${HOME}/.config/nvim;
+set +x;
 
-echo '-----------------------------------------------------------'
-echo '------- CREATE AUTOLOAD AND BUNDLE IN CONFIG NVIM ---------'
-echo '--------------- AND THEN INSTALL PATHOGEN -----------------'
-echo '-----------------------------------------------------------'
+echo "\n-----------------------------------------------------------";
+echo "------- CREATE AUTOLOAD AND BUNDLE IN CONFIG NVIM ---------";
+echo "--------------- AND THEN INSTALL PATHOGEN -----------------";
+echo "-----------------------------------------------------------";
+set -x;
 mkdir -p ${PLUGIN_DIR}/autoload ${PLUGIN_DIR}/bundle && \
 curl -LSso ${PLUGIN_DIR}/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
+set +x;
 
-echo '---------------------------------'
-echo '------- INSTALL PLUGINS ---------'
-echo '---------------------------------\n'
-echo '-----------------------------------------------'
-echo '------- INSTALL COC FOR AUTO-COMPLETE ---------'
-echo '-----------------------------------------------\n'
+echo "\n---------------------------------";
+echo "------- INSTALL PLUGINS ---------";
+echo "---------------------------------";
+echo "-----------------------------------------------";
+echo "------- INSTALL COC FOR AUTO-COMPLETE ---------";
+echo "-----------------------------------------------";
+set -x;
 git clone https://github.com/neoclide/coc.nvim \
   ${PLUGIN_DIR}/bundle/coc.nvim;
+set +x;
 
-echo '-------------------------------'
-echo '------- SETUP FOR COC ---------'
-echo '-------------------------------\n'
+echo "\n-------------------------------";
+echo "------- SETUP FOR COC ---------";
+echo "-------------------------------";
+echo "------- UPDATE COC ---------";
+set -x;
 cd ${PLUGIN_DIR}/bundle/coc.nvim;
 git pull;
 yarn install;
 cd ~;
+set +x;
 
-## telescope is a fuzzy finder
-echo '-----------------------------------'
-echo '------- INSTALL TELESCOPE ---------'
-echo '-----------------------------------\n'
-git clone https://github.com/nvim-telescope/telescope.nvim \
-  ${PLUGIN_DIR}/bundle/telescope.nvim;
-echo '----------------------------------'
-echo '------- UPDATE TELESCOPE ---------'
-echo '----------------------------------\n'
-cd ${PLUGIN_DIR}/bundle/telescope.nvim;
-git pull;
-cd ~;
-
-## The following are requirements for telescope:
-echo '------------------------------------------------'
-echo '------- INSTALL TELESCOPE REQUIREMENTS ---------'
-echo '------------------------------------------------\n'
-git clone https://github.com/nvim-lua/plenary.nvim \
-  ${PLUGIN_DIR}/bundle/plenary;
-git clone https://github.com/nvim-treesitter/nvim-treesitter
-  ${PLUGIN_DIR}/bundle/nvim-treesitter;
-
-## fugitive is for git commands in vim
-echo '----------------------------------'
-echo '------- INSTALL FUGITIVE ---------'
-echo '----------------------------------\n'
-git clone https://github.com/tpope/vim-fugitive \
-  ${PLUGIN_DIR}/bundle/vim-fugitive;
-## update
-echo '---------------------------------'
-echo '------- UPDATE FUGITIVE ---------'
-echo '---------------------------------\n'
-cd ${PLUGIN_DIR}/bundle/vim-fugitive;
-git pull;
-cd ~;
-
-## undo tree is the bomb
-echo '-----------------------------------'
-echo '------- INSTALL UNDO TREE ---------'
-echo '-----------------------------------\n'
-git clone https://github.com/mbbill/undotree ${PLUGIN_DIR}/bundle/undotree;
-## update
-echo '----------------------------------'
-echo '------- UPDATE UNDO TREE ---------'
-echo '----------------------------------'
-cd ${PLUGIN_DIR}/bundle/undotree;
-git pull;
-cd ~;
-
-## The following are for coc autocomplete
-echo '---------------------------------------------------'
-echo '------- INSTALL DEPS FOR COC AUTOCOMPLETE ---------'
-echo '---------------------------------------------------\n'
-echo '------- RUST -------'
+echo "\n---------------------------------------------------";
+echo "------- INSTALL DEPS FOR COC AUTOCOMPLETE ---------";
+echo "------- RUST -------";
+set -x;
 git clone https://github.com/rust-lang/rust.vim.git \
   ${PLUGIN_DIR}/bundle/rust.vim;
-## update
-echo '------- UPDATE ---------'
+set +x;
+echo "------- UPDATE ---------";
+set -x;
 cd ${PLUGIN_DIR}/bundle/rust.vim;
 git pull;
 cd ~;
+set +x;
 
-echo '------- TYPESCRIPT ---------'
+echo "\n------- TYPESCRIPT ---------";
+set -x;
 git clone https://github.com/leafgarland/typescript-vim \
   ${PLUGIN_DIR}/bundle/typescript-vim;
-## update
-echo '------- UPDATE ---------'
+set +x;
+echo "------- UPDATE ---------";
+set -x;
 cd ${PLUGIN_DIR}/bundle/typescript-vim;
 git pull;
 cd ~;
+set +x;
 
-echo '------- JAVASCRIPT ---------'
+echo "\n------- JAVASCRIPT ---------";
+set -x;
 git clone https://github.com/pangloss/vim-javascript.git \
   ${PLUGIN_DIR}/bundle/vim-javascript;
-## update
-echo '------- UPDATE ---------'
+set +x;
+echo "------- UPDATE ---------";
+set -x;
 cd ${PLUGIN_DIR}/bundle/vim-javascript;
 git pull;
 cd ~;
+set +x;
 
-yarn global add neovim;
-
-echo '------- JSX ---------'
+echo "\n------- JSX ---------";
+set -x;
 git clone https://github.com/MaxMEllon/vim-jsx-pretty \
   ${PLUGIN_DIR}/bundle/vim-jsx-pretty;
-## update
-echo '------- UPDATE ---------'
+set +x;
+echo "------- UPDATE ---------"
+set -x;
 cd ${PLUGIN_DIR}/bundle/vim-jsx-pretty;
 git pull;
 cd ~;
+set +x;
 
-echo '------- JSX TYPESCRIPT ---------'
+echo "\n------- JSX TYPESCRIPT ---------"
+set -x;
 git clone https://github.com/peitalin/vim-jsx-typescript \
   ${PLUGIN_DIR}/bundle/vim-jsx-typescript;
-## update
-echo '------- UPDATE ---------'
+set +x;
+echo "------- UPDATE ---------"
+set -x;
 cd ${PLUGIN_DIR}/bundle/vim-jsx-typescript;
 git pull;
 cd ~;
+set +x;
 
-echo '--------------------------------------------------------------------'
-echo '--------------- INSTALL OTHER COC DEPS WITHIN NVIM -----------------'
-echo '---------- AND INSTALL HELP TAGS FOR ALL VIM EXTENSIONS ------------'
-echo 'this might take a minute and there is no status update... be patient'
-echo '--------------------------------------------------------------------'
-sleep 3
+echo "\n-----------------------------------";
+echo "------- INSTALL TELESCOPE ---------";
+echo "--- telescope is a fuzzy finder ---";
+echo "-----------------------------------";
+set -x;
+git clone https://github.com/nvim-telescope/telescope.nvim \
+  ${PLUGIN_DIR}/bundle/telescope.nvim;
+set +x;
 
+echo "\n----------------------------------";
+echo "------- UPDATE TELESCOPE ---------";
+echo "----------------------------------";
+set -x;
+cd ${PLUGIN_DIR}/bundle/telescope.nvim;
+git pull;
+cd ~;
+set +x;
+
+echo "\n------------------------------------------------";
+echo "------- INSTALL TELESCOPE REQUIREMENTS ---------";
+echo "----------- (plenary and treesitter) -----------";
+echo "------------------------------------------------";
+set -x;
+git clone https://github.com/nvim-lua/plenary.nvim \
+  ${PLUGIN_DIR}/bundle/plenary;
+git clone https://github.com/nvim-treesitter/nvim-treesitter \
+  ${PLUGIN_DIR}/bundle/nvim-treesitter;
+set +x;
+
+# fugitive is for git commands in vim
+echo "\n----------------------------------";
+echo "------- INSTALL FUGITIVE ---------";
+echo "----------------------------------";
+set -x;
+git clone https://github.com/tpope/vim-fugitive \
+  ${PLUGIN_DIR}/bundle/vim-fugitive;
+set +x;
+
+echo "\n---------------------------------";
+echo "------- UPDATE FUGITIVE ---------";
+echo "---------------------------------";
+set -x;
+cd ${PLUGIN_DIR}/bundle/vim-fugitive;
+git pull;
+cd ~;
+set +x;
+
+# undo tree is the bomb -- your undo will have so much more power
+echo "\n-----------------------------------";
+echo "------- INSTALL UNDO TREE ---------";
+echo "-----------------------------------";
+set -x;
+git clone https://github.com/mbbill/undotree ${PLUGIN_DIR}/bundle/undotree;
+set +x;
+
+echo "\n----------------------------------";
+echo "------- UPDATE UNDO TREE ---------";
+echo "----------------------------------";
+set -x;
+cd ${PLUGIN_DIR}/bundle/undotree;
+git pull;
+cd ~;
+set +x;
+
+echo "\n---------------------------------------";
+echo "------- YARN INSTALL IN COC VIM -------";
+echo "---------------------------------------";
+set -x;
+yarn install;
+set +x;
+
+echo "\n--------------------------------------------------------------------";
+echo "--------------- INSTALL OTHER COC DEPS WITHIN NVIM -----------------";
+echo "---------- AND INSTALL HELP TAGS FOR ALL VIM EXTENSIONS ------------";
+echo "this might take a minute and there is no status update... be patient";
+echo "- coc-html, coc-tsserver, coc-rust-analyzer, coc-pyright, coc-json -";
+echo "--------------------------------------------------------------------";
+sleep 3;
+
+set -x;
 nvim -c "CocInstall -sync coc-html \
   | CocInstall -sync coc-tsserver \
   | CocInstall -sync coc-rust-analyzer \
@@ -159,3 +211,4 @@ nvim -c "CocInstall -sync coc-html \
   | helptags ALL \
   | q
 ";
+set +x;
